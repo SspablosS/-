a9fbb2a33a3e61110db825475cbeb7e94e58111d
SspablosS <pavelmeshkov2004@gmail.com>
2024-01-21 23:27:36 +0300

    mod

--- generateArray.js
+++ generateArray.js
@@ -16,3 +16,20 @@ const generateRandomNumbers = (length, min, max) =>
     .map(() => +(Math.random() * (max - min) + min).toFixed(0));
 
 export { generateRandomNumbers };
+
+const generate2DArray = (rows, columns) => {
+  const result = [];
+
+  for (let i = 0; i < rows; i += 1) {
+    const row = [];
+    for (let j = 0; j < columns; j += 1) {
+      const randomNUmber = Math.floor(Math.random() * 10) + 1;
+      row.push(randomNUmber);
+    }
+    result.push(row);
+  }
+
+  return result;
+};
+
+export { generate2DArray };
