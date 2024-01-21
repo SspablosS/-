11ce13fa4a711be576c767f42d1f614b4de68180
SspablosS <pavelmeshkov2004@gmail.com>
2024-01-21 22:52:18 +0300

    modified

--- generateArray.js
+++ generateArray.js
@@ -2,3 +2,10 @@ const generateArray = (length, max) =>
   [...new Array(length)].map(() => Math.round(Math.random() * max));
 
 export { generateArray };
+
+const generateRandomRealNumbers = (length, min, max) =>
+  [...new Array(length)]
+    .fill()
+    .map(() => +(Math.random() * (max - min) + min).toFixed(1));
+
+export { generateRandomRealNumbers };
